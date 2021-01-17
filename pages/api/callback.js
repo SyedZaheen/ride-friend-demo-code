da89b51248ff3214853902badddcce9d705b6efc
Chiraag Singh Anand <chiraag@Chiraags-MacBook-Pro.local>
2021-01-17 16:49:56 +0800

    fixed auth0 bug

--- pages/api/callback.js
+++ pages/api/callback.js
@@ -1,8 +1,13 @@
-import auth0 from "./utils/auth0";
+import auth0 from "../../utils/auth0";
 
 export default async function callback(req, res) {
   try {
-    await auth0.handleCallback(req, res, { redirectTo: "/" });
+    await auth0.handleCallback(req, res, {
+      onUserLoaded: async (req, res, session, state) => {
+        return { ...session };
+      },
+      redirectTo: "/",
+    });
   } catch (error) {
     console.error(error);
     res.status(error.status || 400).end(error.message);
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,31 @@
 import React from "react";
-import auth0 from "./api/utils/auth0";
+import auth0 from "../utils/auth0";
 
-class Home extends React.Component {
-  renderContent() {
-    if (this.props.user) {
+export default function Home({ session }) {
+  console.log("this is session prop ", session);
+
+  const renderContent = () => {
+    if (session) {
       return <a href="/api/logout">Logout</a>;
     }
     return <a href="/api/login">login</a>;
-  }
-  render() {
-    return (
+  };
+
+  return (
+    <div>
+      Hello World
+      <div>{renderContent()}</div>
       <div>
-        Hello World
-        <div>{this.renderContent()}</div>
-        <div>
-          Perma logour button: <a href="/api/logout">logout</a>
-        </div>
+        Perma logour button: <a href="/api/logout">logout</a>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default Home;
-
-export async function getServerSideProps(context) {
-  const session = await auth0.getSession(context.req);
-  return {
-    props: {
-      user: session?.user || null,
-    },
-  };
+export async function getServerSideProps({ req, res }) {
+  const session = await auth0.getSession(req);
+  if (session) {
+    return { props: { user: session?.user || null } };
+  }
+  return { props: {} };
 }
