1aef6e92cca16551203f1509780d9f653edc998a
Chiraag Singh Anand <chiraag@Chiraags-MacBook-Pro.local>
2021-01-24 18:50:05 +0800

    Changed db secret env variable name to expose to browser

--- utils/graphqlClient.js
+++ utils/graphqlClient.js
@@ -5,7 +5,7 @@ const endpoint = "https://graphql.fauna.com/graphql";
 // ... or create a GraphQL client instance to send requests
 const graphqlClient = new GraphQLClient(endpoint, {
   headers: {
-    authorization: `Bearer ${process.env.DB_SECRET}`,
+    authorization: `Bearer ${process.env.NEXT_PUBLIC_DB_SECRET}`,
   },
 });
 
